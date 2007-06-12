1cd4d4e80844ea3ee8d98e14e0283690cf3d417c
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-06-12 22:25:43 +0000

    Add in Test_Util::parse()
    git-svn-id: https://svn.domain51.com/test/trunk@4 54d25d7b-d40e-47af-a99d-8d4037bc02b6

---
+++ src/Test/Util.php
@@ -0,0 +1,34 @@
+<?php
+
+class Test_Util
+{
+    public function __construct()
+    {
+        
+    }
+    
+    static public function parse($code)
+    {
+        $exploded_code = explode("\n", $code);
+        if (count($exploded_code) == 1) {
+            // simple regex
+            preg_match('/<\?(php)?(.*)\?>/', $code, $matches);
+            return $matches[2];
+        }
+        
+        // first and last line for php process instructions and strip them
+        $first_line = array_shift($exploded_code);
+        preg_match('/^<\?(php)?(.*)(\?>)?$/', $first_line, $matches);
+        if (!empty($matches[2])) {
+            array_unshift($exploded_code, $matches[2]);
+        }
+        
+        $last_line = array_pop($exploded_code);
+        preg_match('/^(.*)(\?>)$/', $last_line , $matches);
+        if (!empty($matches[1])) {
+            array_push($exploded_code, $matches[1]);
+        }
+        
+        return trim(implode("\n", $exploded_code));
+    }
+}
