ab9c5115505aedf3679118f811eb0918ca6d9b0d
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-06-12 22:27:48 +0000

    Refactor to make use of new parser
    git-svn-id: https://svn.domain51.com/test/trunk@5 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/Test/Section/Setup.php
+++ src/Test/Section/Setup.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'Test/Section.php';
+require_once 'Test/Util.php';
 
 class Test_Section_Setup extends Test_Section
 {
@@ -14,25 +15,9 @@ class Test_Section_Setup extends Test_Section
      */
     public function run($source)
     {
-        $exploded_source = explode("\n", $source);
-        $source = array_shift($exploded_source);
-        
-        preg_match('/^<\?(php)?(.*)\?>$/', $source, $matches);
-        if (!empty($matches[2])) {
-            $exploded_source = array_unshift($exploded_source, $matches[2]);
-        }
-        $source = array_pop($exploded_source);
-        unset($matches);
-        preg_match('/^(.*)(\?>)$/', $source, $matches);
-        if (!empty($matches[1])) {
-            $exploded_source = array_push($exploded_source, $matches[1]);
-        }
-        
-        $source_fragment = implode("\n", $exploded_source);
-        $source = "<?php\n" .
+        return "<?php\n" .
             $this->php_fragment . "\n" .
-            $source_fragment . "\n" .
+            Test_Util::parse($source) . "\n" .
             "?>";
-        return $source;
     }
 }
