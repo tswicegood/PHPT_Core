06d8cceb9baafd793a9be9e432314d8668451dbd
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-06-15 22:20:17 +0000

    Add in False assertion and tests
    git-svn-id: https://svn.domain51.com/test/trunk@25 54d25d7b-d40e-47af-a99d-8d4037bc02b6

---
+++ src/Test/Assert/False.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'Test/Assertion.php';
+
+class Test_Assert_False implements Test_Assertion
+{
+    private $_message = null;
+    private $_status = false;
+    private $_value = null;
+    
+    public function __construct($value, $message = null)
+    {
+        $this->_message = $message;
+        $this->_value = $value;
+        $this->_status = (bool)$value;
+    }
+    
+    public function getStatus()
+    {
+        return !$this->_status;
+    }
+    
+    public function getMessage()
+    {
+        if (!is_null($this->_message)) {
+            $value = var_export($this->_value, true);
+            return sprintf($this->_message, $value);
+        }
+        
+        if ($this->getStatus()) {
+            if (is_array($this->_value)) {
+                $value = str_replace("\n", '', var_export($this->_value, true));
+            } else {
+                $value = var_export($this->_value, true);
+            }
+            return "value [{$value}] is false";
+        } else {
+            return 'value [' . var_export($this->_value, true) . '] is not false';
+        }
+    }
+}
