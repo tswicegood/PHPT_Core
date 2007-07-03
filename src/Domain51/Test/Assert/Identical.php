07f088812719bfe9f262499017dd503132d8e9ce
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-07-03 23:21:43 +0000

    Update identical assertion to allow for sprintf messages
    git-svn-id: https://svn.domain51.com/test/trunk@42 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/Domain51/Test/Assert/Identical.php
+++ src/Domain51/Test/Assert/Identical.php
@@ -6,13 +6,15 @@ class Domain51_Test_Assert_Identical implements Domain51_Test_Assertion
 {
     private $_one = null;
     private $_two = null;
-    private $_message = null;
+    private $_message = 'values [%s] and [%s] %s identical';
     
     public function __construct($one, $two, $message = null)
     {
         $this->_one = $one;
         $this->_two = $two;
-        $this->_message = $message;
+        if (!is_null($message)) {
+            $this->_message = $message;
+        }
     }
     
     public function getStatus()
@@ -22,12 +24,7 @@ class Domain51_Test_Assert_Identical implements Domain51_Test_Assertion
     
     public function getMessage()
     {
-        if (!is_null($this->_message)) {
-            return $this->_message;
-        }
-        
-        $message = 'values [%s] and [%s] %s identical';
-        return sprintf($message,
+        return sprintf($this->_message,
                        var_export($this->_one, true),
                        var_export($this->_two, true),
                        $this->getStatus() ? 'are' : 'are not'
