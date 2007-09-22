f78c30b2742a21ada6159c59cb049f3d8dc3acfc
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-09-22 15:43:11 +0000

    Add in bare-bones Section_Post
    git-svn-id: https://svn.domain51.com/test/trunk@91 54d25d7b-d40e-47af-a99d-8d4037bc02b6

---
+++ src/Domain51/Test/Section/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+class Domain51_Test_Section_Post implements Domain51_Test_Section, Domain51_Test_Section_EnvModifier
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function run(Domain51_Test_Case $case)
+    {
+        
+    }
+    
+    public function modifyEnv(Domain51_Test_Section_Env $env)
+    {
+        
+    }
+}
