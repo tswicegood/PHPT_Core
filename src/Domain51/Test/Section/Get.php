26b2866345b3585d3122ae206d0b09b30724faf5
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-09-23 22:21:23 +0000

    Add in an @internal comment on why run() doesn't do anything
    git-svn-id: https://svn.domain51.com/test/trunk@104 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/Domain51/Test/Section/Get.php
+++ src/Domain51/Test/Section/Get.php
@@ -7,6 +7,11 @@ class Domain51_Test_Section_Get implements Domain51_Test_Section, Domain51_Test_
         
     }
     
+    /**
+     * @internal This method is here to satisfy the {@link Domain51_Test_Section} interface.
+     *           Section_Get's main purpose is modify the environment to insure the correct
+     *           REQUEST_METHOD and a QUERY_STRING (if any) is in place.
+     */
     public function run(Domain51_Test_Case $case)
     {
         
