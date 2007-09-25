83878c381302e8ded6e0e6bde45d6352789d8248
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-09-25 15:51:02 +0000

    Add skeleton of Section_Exceptregex
    git-svn-id: https://svn.domain51.com/test/trunk@124 54d25d7b-d40e-47af-a99d-8d4037bc02b6

---
+++ src/Domain51/Test/Section/Expectregex.php
@@ -0,0 +1,14 @@
+<?php
+
+class Domain51_Test_Section_Expectregex implements Domain51_Test_Section_Runnable
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function run(Domain51_Test_Case $case)
+    {
+        
+    }
+}
