a4d3f003556e85b6f8728467c56ef940fe8f2880
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-09-30 02:42:15 +0000

    Remove duplicate foreach
    git-svn-id: https://svn.domain51.com/test/trunk@168 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/Domain51/Test/Case/Parser.php
+++ src/Domain51/Test/Case/Parser.php
@@ -9,44 +9,47 @@ class Domain51_Test_Case_Parser
     
     public function parse($file)
     {
-        $sections = array();
+        $raw_sections = array();
         $lines = file($file);
-        $current_section = null;
+        $section_name = '';
+        $section_data = '';
+        
         foreach ($lines as $line) {
             if (preg_match('/^--([^-]+)--$/', $line, $matches)) {
-                $current_section = 'Domain51_Test_Section_' . ucfirst(strtolower($matches[1]));
-                $sections[$current_section] = array();
+                if (!empty($section_name)) {
+                    $raw_sections[] = new $section_name($section_data);
+                }
+                
+                $section_name = 'Domain51_Test_Section_' . ucfirst(strtolower($matches[1]));
+                $section_data = '';
                 continue;
             }
             
-            if (is_null($current_section)) {
+            if (is_null($section_name)) {
                 // nothing to do here yet
                 continue;
             }
             
-            $sections[$current_section][] = $line;
+            if (!empty($section_data)) {
+                $section_data .= "\n";
+            }
+            $section_data .= trim($line);
         }
         
-        // @todo refactor so only one foreach is done
-        foreach ($sections as $name => $data) {
-            $sections[] = new $name(trim(implode("\n", $data)));
-            unset($sections[$name]);
-        }
-        
-        $list = new Domain51_Test_SectionList($sections);
+        $sections = new Domain51_Test_SectionList($raw_sections);
         
         // @todo move this over to Domain51_Test_Case_Validator
-        if (!$list->has('TEST')) {
+        if (!$sections->has('TEST')) {
             throw new Domain51_Test_Case_Parser_InvalidTestCaseException('missing TEST section');
         }
         
-        if (!$list->has('FILE')) {
+        if (!$sections->has('FILE')) {
             throw new Domain51_Test_Case_Parser_InvalidTestCaseException('missing FILE section');
         }
         
-        $list->FILE->filename = dirname($file) . '/' . basename($file, '.phpt') . '.php';
+        $sections->FILE->filename = dirname($file) . '/' . basename($file, '.phpt') . '.php';
         
-        return new Domain51_Test_Case($list);
+        return new Domain51_Test_Case($sections);
     }
 }
 
