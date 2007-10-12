ba04b8ecb6951e6eaf7ed3fe3fc9ff985a431f20
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-10-12 21:21:08 +0000

    Internal refactoring - change _runner to _driver to reflect its new name
    git-svn-id: https://svn.domain51.com/test/trunk@250 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/CodeRunner.php
+++ src/PHPT/CodeRunner.php
@@ -2,31 +2,31 @@
 
 class PHPT_CodeRunner
 {
-    private $_runner = null;
+    private $_driver = null;
     
     public function __construct($type = 'Proc')
     {
         $runner_name = 'PHPT_CodeRunner_Driver_' . $type;
-        $this->_runner = new $runner_name($this);
+        $this->_driver = new $runner_name($this);
     }
     
     public function __set($key, $value)
     {
-        $this->_runner->$key = $value;
+        $this->_driver->$key = $value;
     }
     
     public function __get($key)
     {
-        return $this->_runner->$key;
+        return $this->_driver->$key;
     }
     
     public function run($filename)
     {
-        return $this->_runner->run($filename);
+        return $this->_driver->run($filename);
     }
     
     public function validate()
     {
-        $this->_runner->validate();
+        $this->_driver->validate();
     }
 }
