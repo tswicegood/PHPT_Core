caacdbf96ab80a42656300622b4c394d9eb5365d
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-10-20 23:51:11 +0000

    Switch to @php_bin@ and use include_path
    git-svn-id: https://svn.domain51.com/test/Core/trunk@360 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- scripts/phpt.php
+++ scripts/phpt.php
@@ -1,10 +1,10 @@
-#!/usr/local/bin/php -d apc.enable_cli=1
+#!@php_bin@
 <?php
 /**
  * @internal this is all devel level code just for testing out on other systems
  */
 
-require_once dirname(__FILE__) . '/../src/PHPT/Framework.php';
+require_once 'PHPT/Framework.php';
 
 $controller = new PHPT_Controller_CLI();
 $controller->run($argv);
