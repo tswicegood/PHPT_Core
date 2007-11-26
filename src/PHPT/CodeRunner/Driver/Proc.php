dd09af5696a190a60aeda46f6b5b0da4ae9a68df
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-11-26 21:09:15 +0000

    Refactoring: Extract _runCode() method
    git-svn-id: https://svn.domain51.com/test/Core/trunk@534 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/CodeRunner/Driver/Proc.php
+++ src/PHPT/CodeRunner/Driver/Proc.php
@@ -2,6 +2,8 @@
 
 class PHPT_CodeRunner_Driver_Proc extends PHPT_CodeRunner_Driver_Abstract
 {
+    private $_pipes = array();
+
     public function run($filename)
     {
         $this->filename = $filename;
@@ -9,6 +11,54 @@ class PHPT_CodeRunner_Driver_Proc extends PHPT_CodeRunner_Driver_Abstract
         $result = new PHPT_CodeRunner_Result();
         $result->filename = $filename;
         
+        $proc = $this->_runCode();
+        
+       
+        if (!is_null($this->stdin)) {
+            fwrite($this->_pipes[0], (string)$this->stdin);
+        }
+        fclose($this->_pipes[0]);
+        
+        $data = '';
+        
+        while (true) {
+            /* hide errors from interrupted syscalls */
+            $read = $this->_pipes;
+            $except = $write = null;
+            $n = @stream_select($read, $write, $except, $this->timeout);
+            
+            if ($n === 0) {
+                proc_terminate($proc);
+                throw new PHPT_CodeRunner_TimeoutException($this->_caller);
+                
+            } elseif ($n > 0) {
+                $error_pipe = fread($this->_pipes[2], 8192);
+                if (!empty($error_pipe)) {
+                    throw new PHPT_CodeRunner_ExecutionException($error_pipe);
+                }
+
+                $line = fread($this->_pipes[1], 8192);
+                if (strlen($line) == 0) {
+                    /* EOF */
+                    fclose($this->_pipes[1]);
+                    break;
+                }
+                $data .= $line;
+            }
+        }
+    
+        $exitcode = trim(fread($this->_pipes[3], 5));
+        fclose($this->_pipes[3]);
+        
+        proc_close($proc);
+        
+        $result->output = $data;
+        $result->exitcode = $exitcode;
+        return $result;
+    }
+
+    private function _runCode()
+    {
         $command = new PHPT_CodeRunner_CommandLine($this);
         if (!empty($this->command_line)) {
             $command->executable = $this->command_line;
@@ -22,62 +72,22 @@ class PHPT_CodeRunner_Driver_Proc extends PHPT_CodeRunner_Driver_Abstract
                 2 => array('pipe', 'w'),
                 3 => array('pipe', 'w'), // pipe to write exit code to
             ),
-            $pipes,
+            $this->_pipes,
             null,
             $this->environment,
             array(
             //    'supress_errors' => true,
             )
         );
-        
+
         // @todo figure out how to test
         if ($proc == false) {
             throw new PHPT_Exception_InvalidStateException(
                 'proc_open returned false in ' . __METHOD__
             );
         }
-        
-        if (!is_null($this->stdin)) {
-            fwrite($pipes[0], (string)$this->stdin);
-        }
-        fclose($pipes[0]);
-        
-        $data = '';
-        
-        while (true) {
-            /* hide errors from interrupted syscalls */
-            $read = $pipes;
-            $except = $write = null;
-            $n = @stream_select($read, $write, $except, $this->timeout);
-            
-            if ($n === 0) {
-                proc_terminate($proc);
-                throw new PHPT_CodeRunner_TimeoutException($this->_caller);
-                
-            } elseif ($n > 0) {
-                $error_pipe = fread($pipes[2], 8192);
-                if (!empty($error_pipe)) {
-                    throw new PHPT_CodeRunner_ExecutionException($error_pipe);
-                }
-
-                $line = fread($pipes[1], 8192);
-                if (strlen($line) == 0) {
-                    /* EOF */
-                    fclose($pipes[1]);
-                    break;
-                }
-                $data .= $line;
-            }
-        }
-    
-        $exitcode = trim(fread($pipes[3], 5));
-        fclose($pipes[3]);
-        
-        proc_close($proc);
-        
-        $result->output = $data;
-        $result->exitcode = $exitcode;
-        return $result;
+ 
+        return $proc;
     }
     
     public function validate()
