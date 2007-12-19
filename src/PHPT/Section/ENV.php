cc7b29ba97b47ae0d3a708be1994f759413bfd27
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2007-12-19 04:00:47 +0000

    Refactor code into more legible version of itself
    git-svn-id: https://svn.domain51.com/test/Core/trunk@595 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/Section/ENV.php
+++ src/PHPT/Section/ENV.php
@@ -17,25 +17,41 @@ class PHPT_Section_ENV extends PHPT_Section_ModifiableAbstract
     public function __construct($data = '')
     {
         parent::__construct($data);
+        $this->_setDefaultValues();
+        if (!empty($data)) {
+            $this->_parseProvidedData(
+                $this->_evaluateProvidedData($data)
+            );
+        }
+    }
+
+    private function _setDefaultValues()
+    {
         $this->data = $_ENV;
         foreach ($this->_default_values_to_empty as $key_name) {
             $this->data[$key_name] = '';
         }
-        
+    }
+
+    private function _parseProvidedData($data)
+    {
+        $lines = explode("\n", $data);
+        foreach ($lines as $line) {
+            $pair = explode('=', trim($line), 2);
+            $this->data[$pair[0]] = $pair[1];
+        }
+    }
+
+    private function _evaluateProvidedData($data)
+    {
         $data = trim($data);
         $php = new PHPT_Util_Code($data);
         if ($php->isValid()) {
             $data = $php->evaluate();
         }
-        if (!empty($data)) {
-            $lines = explode("\n", $data);
-            foreach ($lines as $line) {
-                $pair = explode('=', trim($line), 2);
-                $this->data[$pair[0]] = $pair[1];
-            }
-        }
+        return $data;
     }
-    
+
     public function run(PHPT_Case $case)
     {
         $this->data['PATH_TRANSLATED'] = $this->data['SCRIPT_FILENAME'] = $case->filename;
