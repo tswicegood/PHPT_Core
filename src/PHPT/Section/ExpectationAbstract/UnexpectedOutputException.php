f10f13fe168cddd1d8c340aa9f7dce9144df76e2
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2008-01-01 04:02:51 +0000

    Refactor internals of creating files by UnexpectedOutputException
    git-svn-id: https://svn.domain51.com/test/Core/trunk@609 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/Section/ExpectationAbstract/UnexpectedOutputException.php
+++ src/PHPT/Section/ExpectationAbstract/UnexpectedOutputException.php
@@ -9,29 +9,16 @@ abstract class PHPT_Section_ExpectationAbstract_UnexpectedOutputException
     
     public function __construct(PHPT_Case $case, $expected)
     {
+        $this->_case = $case;
         $this->_wanted = $expected;
-        $this->_actual = $case->output;
-        
-        file_put_contents(
-            $case->filename . '.exp',
-            $this->_wanted
-        );
-        
-        file_put_contents(
-            $case->filename . '.out',
-            $case->output
-        );
-        
-        file_put_contents(
-            $case->filename . '.diff',
-            $this->getDiff()
-        );
-        
-        file_put_contents(
-            $case->filename . '.log',
-            $this->_getLog($case, $expected)
-        );
-        parent::__construct($case, $this->_message);
+        $this->_actual = $this->_case->output;
+
+        $this->_createExpectationFile();
+        $this->_createOutputFile();
+        $this->_createDiffFile();
+        $this->_createLogFile();
+
+        parent::__construct($this->_case, $this->_message);
     }
     
     public function __toString()
@@ -44,12 +31,49 @@ abstract class PHPT_Section_ExpectationAbstract_UnexpectedOutputException
         return (string)new PHPT_Util_Diff($this->_wanted, $this->_actual);
     }
     
+    
+
+    protected function _createExpectationFile() {
+        file_put_contents(
+            $this->_case->filename . '.exp',
+            $this->_wanted
+        );
+    }
+
+    protected function _createOutputFile()
+    {
+        file_put_contents(
+            $this->_case->filename . '.out',
+            $this->_case->output
+        );
+    }
+
+    protected function _createDiffFile()
+    {
+        file_put_contents(
+            $this->_case->filename . '.diff',
+            $this->getDiff()
+        );
+    }
+
+    protected function _createLogFile()
+    {
+        file_put_contents(
+            $this->_case->filename . '.log',
+            $this->_getLog($this->_case, $this->_wanted)
+        );
+    }
+
+    /**
+     * @todo refactor into own class
+     */
     private function _getLog(PHPT_Case $case, $expected)
     {
         return "---- EXPECTED OUTPUT\n"
             . $expected . "\n"
             . "---- ACTUAL OUTPUT\n"
-            . $case->output . "\n"
+            . $this->_case->output . "\n"
             . "---- FAILED\n";
     }
 }
+
