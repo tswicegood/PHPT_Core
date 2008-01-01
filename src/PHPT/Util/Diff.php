c18c907fe76940fe24d3cbe57021aaaff2150faf
tswicegood <tswicegood@54d25d7b-d40e-47af-a99d-8d4037bc02b6>
2008-01-01 18:55:19 +0000

    Remove class delcaration from Diff - already there in another file
    git-svn-id: https://svn.domain51.com/test/Core/trunk@614 54d25d7b-d40e-47af-a99d-8d4037bc02b6

--- src/PHPT/Util/Diff.php
+++ src/PHPT/Util/Diff.php
@@ -50,10 +50,3 @@ class PHPT_Util_Diff
     }
 }
 
-class PHPT_Util_Diff_InvalidParameter extends Exception
-{
-    public function __construct($parameter_name)
-    {
-        parent::__construct('provided $' . $parameter_name . ' value is not a valid string');
-    }
-}
