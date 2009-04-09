7a1ccfe41b65d2eddda063d9bfdc1cb7c88c7d1d
Travis Swicegood <development@domain51.com>
2009-04-09 17:47:21 -0500

    Prevent a new E_WARNING in PHP 5.3

--- src/PHPT/Section/POST.php
+++ src/PHPT/Section/POST.php
@@ -21,6 +21,9 @@ class PHPT_Section_POST implements PHPT_Section_RunnableBefore, PHPT_Section_ENV
     
     public function run(PHPT_Case $case)
     {
+        if (empty($this->file)) {
+            return;
+        }
         file_put_contents($this->file, $this->raw_data);
     }
     
